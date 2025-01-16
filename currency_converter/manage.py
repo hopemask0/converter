ae7d529f6757e818c1965123f5889d8039d1b3d0
Nadya Mazko <nadya.mazko.04@mail.ru>
2025-01-16 21:02:10 +0300

    change the path

--- currency_converter/manage.py
+++ currency_converter/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'currency_converter.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'currency_converter.currency_converter.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
