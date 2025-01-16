e3aea6256f769b94a02fc8707a456d3d0bb5bc36
Nadya Mazko <nadya.mazko.04@mail.ru>
2025-01-16 21:35:59 +0300

    Fix cconverter module path

--- currency_converter/currency_converter/urls.py
+++ currency_converter/currency_converter/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('cconverter.urls')),
+    path('', include('currency_converter.cconverter.urls')),
 ]
