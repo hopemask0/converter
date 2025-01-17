57597ae931937c0a7274052e0c67a0e4c45399df
Nadya Mazko <nadya.mazko.04@mail.ru>
2025-01-17 20:39:15 +0300

    add error message

--- currency_converter/cconverter/utils.py
+++ currency_converter/cconverter/utils.py
@@ -1,16 +1,26 @@
 import requests
 
+
 def get_exchange_rate(source_currency, target_currency):
-    api_url = f'https://www.cbr-xml-daily.ru/daily_json.js'
+    api_url = 'https://www.cbr-xml-daily.ru/daily_json.js'
     response = requests.get(api_url)
     data = response.json()
+
+    # Проверка, что source_currency и target_currency существуют в данных
+    if source_currency != 'RUB' and source_currency not in data['Valute']:
+        raise ValueError(f"Валюта {source_currency} не найдена.")
+    if target_currency != 'RUB' and target_currency not in data['Valute']:
+        raise ValueError(f"Валюта {target_currency} не найдена.")
+
+    # Получение курсов валют
     if source_currency != 'RUB':
         sc = data['Valute'][source_currency]['Value']
     else:
-        sc = 1
+        sc = 1  # RUB всегда равен 1
+
     if target_currency != 'RUB':
         tc = data['Valute'][target_currency]['Value']
     else:
-        tc = 1
+        tc = 1  # RUB всегда равен 1
 
-    return sc/tc
+    return sc / tc
--- currency_converter/cconverter/views.py
+++ currency_converter/cconverter/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import ConversionForm
+from django.contrib import messages
 from .models import ConversionHistory
 from .utils import get_exchange_rate
+
 
 def convert_currency(request):
     if request.method == 'POST':
@@ -10,17 +12,27 @@ def convert_currency(request):
             source_currency = form.cleaned_data['source_currency']
             target_currency = form.cleaned_data['target_currency']
             amount = float(form.cleaned_data['amount'])
-            rate = get_exchange_rate(source_currency, target_currency)
-            converted_amount = amount * rate
-            ConversionHistory.objects.create(
-                source_currency=source_currency,
-                target_currency=target_currency,
-                amount=amount,
-                converted_amount=converted_amount
-            )
-            return render(request, 'converter/result.html', {'converted_amount': converted_amount})
+
+            try:
+                rate = get_exchange_rate(source_currency, target_currency)
+                converted_amount = amount * rate
+
+                # Сохранение в историю
+                ConversionHistory.objects.create(
+                    source_currency=source_currency,
+                    target_currency=target_currency,
+                    amount=amount,
+                    converted_amount=converted_amount
+                )
+
+                return render(request, 'converter/result.html', {'converted_amount': converted_amount})
+            except ValueError as e:
+                # Показ сообщения об ошибке
+                messages.error(request, str(e))
+                return redirect('convert_currency')
     else:
         form = ConversionForm()
+
     return render(request, 'converter/convert.html', {'form': form})
 
 
